a089fb6952f9a45e5818c6544912021b9efddab1
Renato <rpgouveia@gmail.com>
2024-09-09 18:35:17 -0300

    doc: add docstring to main.py and database.py

--- core/database.py
+++ core/database.py
@@ -8,6 +8,15 @@ engine: Engine = create_engine("sqlite:///insurance.db", echo=False)
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def connect_db() -> Session:
+    """
+    Esta função faz a conexão com o banco de dados.
+
+    Parâmetros:
+        Nenhum
+    
+    Retorno:
+        db (Session): Retorna um objeto Session.
+    """
     db: Session = session_local()
     return db
 
--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from core.database import connect_db
 
 
 def main() -> None:
+    """
+    Esta função orquestra o fluxo de operações do programa de cadastro de clientes.
+
+    Parâmetros:
+        Nenhum
+    
+    Retorno:
+        Não há retorno, apenas executa a operação escolhida pelo usuário no terminal.
+    """
     db: Session = connect_db()
 
     status: bool = True
