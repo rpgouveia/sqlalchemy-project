949908caae0b69d8046862aec02699fe03f2b9d9
Renato Gouveia <rpgouveia@gmail.com>
2024-11-10 16:34:50 -0300

    feat: update save_new_user function

--- core/crud.py
+++ core/crud.py
@@ -1,6 +1,6 @@
 from typing import List, Union
 from sqlalchemy.orm import Session
-from core.models import Client
+from core.models import Client, Users
 
 
 def create_client(db: Session, client: Client) -> Client:
@@ -27,5 +27,32 @@ def update_client(db: Session, client: Client) -> Client:
 def delete_client(db: Session, client_id: int) -> None:
     client: Union[Client | None] = db.query(Client).filter(Client.id == client_id).first()
     db.delete(client)
+    db.commit()
+
+
+def create_user(db: Session, user: Users) -> Users:
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+
+def get_all_users(db: Session) -> List[Users]:
+    return db.query(Users).all()
+
+
+def get_user(db: Session, user_id: int) -> Union[Users | None]:
+    return db.query(Users).filter(Users.id == user_id).first()
+
+
+def update_user(db: Session, user: Users) -> Users:
+    db.commit()
+    db.refresh(user)
+    return user
+
+
+def delete_user(db: Session, user_id: int) -> None:
+    user: Union[Users | None] = db.query(Users).filter(Users.id == user_id).first()
+    db.delete(user)
     db.commit()
 
--- gui/user_input_handlers.py
+++ gui/user_input_handlers.py
@@ -1,18 +1,93 @@
-
-
-"""
-Users(
-    username="user", 
-    fullname="Usuário", 
-    phone="00000000001", 
-    email="user@mail.com", 
-    password="user123",
-    access_level="user"
+from PySide6.QtWidgets import (
+    QMessageBox,
 )
-"""
+from core.validations import (
+    validate_phone,
+    validate_email,
+)
+from core.crud import (
+    create_user,
+)
+from core.models import Users
+from sqlalchemy.exc import IntegrityError
+
 
 def save_new_user(self):
-    ...
+    """ Salva o usuário no banco de dados após validar os dados """
+    try:
+        username = self.new_username_input.text()
+        fullname = self.new_fullname_input.text()
+        phone = validate_phone(self.new_phone_input.text())
+        email = validate_email(self.new_email_input.text())
+
+        password = self.new_password_input.text()
+        if not password:
+            QMessageBox.warning(
+                self,
+                "Erro de Validação",
+                "A senha não pode estar vazia!"
+            )
+            return
+
+        if not all([username, fullname, phone, email]):
+            QMessageBox.warning(
+                self,
+                "Erro de Validação",
+                "Todos os campos são obrigatórios!"
+            )
+            return
+
+        access_level = "admin" if self.is_admin_input.isChecked() else "user"
+
+        new_user = Users(
+            username=username,
+            fullname=fullname,
+            phone=phone,
+            email=email,
+            access_level=access_level
+        )
+
+        new_user.set_password(plaintext_password=password)
+
+        try:
+            create_user(self.db, new_user)
+            
+            QMessageBox.information(
+                self,
+                "Sucesso",
+                f"Usuário {username} criado com sucesso!"
+            )
+
+            self.new_username_input.clear()
+            self.new_fullname_input.clear()
+            self.new_phone_input.clear()
+            self.new_email_input.clear()
+            self.new_password_input.clear()
+            self.is_admin_input.setChecked(False)
+
+        except IntegrityError:
+            self.db.rollback()
+            QMessageBox.critical(
+                self,
+                "Erro",
+                "Usuário não pode ser criado.\n"
+                "Usuário, telefone ou email já existem no sistema."
+            )
+        except Exception as e:
+            self.db.rollback()
+            QMessageBox.critical(
+                self,
+                "Erro",
+                f"Erro ao criar usuário: {str(e)}"
+            )
+
+    except Exception as e:
+        self.db.rollback()
+        QMessageBox.critical(
+            self,
+            "Erro",
+            f"Erro ao criar usuário: {str(e)}"
+        )
 
 
 def list_users(self):
